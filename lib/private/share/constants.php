a880633404b2a45791d2ff0f4844d7e8935edf2b
Joas Schilling <nickvergessen@owncloud.com>
2015-07-02 11:37:19 +0200

    Fix the path for users which have an exception for a group share

--- lib/private/share/constants.php
+++ lib/private/share/constants.php
@@ -27,6 +27,7 @@ class Constants {
 
 	const SHARE_TYPE_USER = 0;
 	const SHARE_TYPE_GROUP = 1;
+	const SHARE_TYPE_GROUP_USER = 2;
 	const SHARE_TYPE_LINK = 3;
 	const SHARE_TYPE_EMAIL = 4;   // ToDo Check if it is still in use otherwise remove it
 	const SHARE_TYPE_CONTACT = 5; // ToDo Check if it is still in use otherwise remove it
